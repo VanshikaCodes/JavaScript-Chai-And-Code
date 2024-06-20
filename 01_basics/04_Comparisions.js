09ebcf058da181bf3aa250b0fe5906c8e192155d
VanshikaCodes <153213173+VanshikaCodes@users.noreply.github.com>
2024-06-20 07:47:56 +0000

    Compaisions

---
+++ 01_basics/04_Comparisions.js
@@ -0,0 +1,17 @@
+// console.log("2" > 1);            //1
+// console.log("2" >= 1);           //1
+// console.log("2" < 1);            //0
+// console.log("2" == 1);           //0
+
+// console.log(null > 0);           //1     //0
+// console.log(null >= 0);          //1
+// console.log(null < 0);           //0
+// console.log(null == 0);          //0
+
+// console.log(undefined > 0);      //0
+// console.log(undefined >= 0);     //0
+// console.log(undefined < 0);      //1      //0
+// console.log(undefined == 0);     //0
+
+// **********     "==="        #########
+console.log("2" === 1) 
