a2bdff84d70b698e583db943fad6df8be295a258
VanshikaCodes <153213173+VanshikaCodes@users.noreply.github.com>
2024-06-20 07:37:05 +0000

    Variables In JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "vanshika@gmail.com"
+var accountPassword = "3333"
+accountCity = "Jaipur"
+
+// accountId = 123
+
+// console.log(accountId);  //can't be redeclared
+
+accountEmail = "ishu@gmail.com"   
+var accountPassword = "333"   //block-scoped, 
+accountCity = "Agra"
+
+let variable;
+// const value;
+var tyi;
+console.table([accountId, accountEmail, accountPassword, accountCity, variable, tyi])
