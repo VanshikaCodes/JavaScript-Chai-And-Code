c43c02c4008ea7d4575c99e8b126f4522e3a0b4f
VanshikaCodes <153213173+VanshikaCodes@users.noreply.github.com>
2024-06-20 09:20:44 +0000

    Strings

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -1,6 +1,8 @@
 let name = "Vanshika"                      //TypeOf name will be string
 let value = 50
 
+// console.log(name.length);                   //Not recommened
+
 console.log(name + " is and repo is " + value);
 
 console.log(`Hello, my name is ${name} and my repo value is ${value}`)
