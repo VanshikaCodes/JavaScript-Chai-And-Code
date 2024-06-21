517ff25c6a57473bad27312fe9f5e0f3c972e0af
VanshikaCodes <153213173+VanshikaCodes@users.noreply.github.com>
2024-06-21 13:51:03 +0000

    Functions

---
+++ 03_basics/02_functions_and_others.js
@@ -0,0 +1,61 @@
+// Rest Operator/ Spread Operator
+// function calculateCartValue1(...num) {                   
+//     return num
+// }
+// console.log(calculateCartValue1(23, 78, 59, 20));
+
+// function calculateCartValue2(val1, val2, ...num) {                    
+//     return num
+// }
+// console.log(calculateCartValue2(23, 78, 59, 20));                 // Will return  [ 59, 20 ] as val1 will store 23, val2 = 78
+
+
+// const user = {
+//     username : "xyz",
+//     price : 199
+// }
+
+// user.greeting = function() {                               //Object's function
+//     console.log("Hello buddyyy");
+// }
+
+// console.log((user));
+
+
+// function handleObject(anyobject) {
+//     console.log(`The item ${anyobject.username} has price ${anyobject.price}`);
+// }
+// handleObject(user)
+// handleObject({
+//     username : "sam",
+//     price : 122
+// })
+
+
+// const arr = [100, 200, 300, 400]
+
+// function returnSecondValue(anyArray) {
+//     return anyArray[1]
+// }
+
+// console.log(returnSecondValue(arr));
+
+
+// const addOne = function(num) {                    //Also called as expression
+//    return num + 1
+// }
+// console.log(addOne(4));
+
+if(true){
+    console.log("hiii");
+    const username = "ishu"
+   function website(){
+        const web = "youtube"
+        console.log(username + web);
+   }
+//    console.log(web);
+
+   website()
+}
+// console.log(username);
+
