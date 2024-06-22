1368179693a199340e752c558bcf70b4b1c1b526
VanshikaCodes <153213173+VanshikaCodes@users.noreply.github.com>
2024-06-22 06:05:58 +0000

    If Else

--- 04_Control_Flow/01.js
+++ 04_Control_Flow/01.js
@@ -1,8 +1,11 @@
 
-let age = 2 
-if(age > 18) {
-    console.log("Eligible to vote");
-}
-else {
-    console.log("Not Eligible");
-}
+let age = 20
+// if(age > 18) {
+//     console.log("Eligible to vote");
+// }
+// else {
+//     console.log("Not Eligible");
+// }
+
+// Valid but not recommened 
+if(age > 18) console.log("Vote"), console.log("DL"), console.log("PanCard");
