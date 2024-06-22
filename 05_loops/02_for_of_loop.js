b27e46a523f4cf3616b3746fd93d2114e1aa1e09
VanshikaCodes <153213173+VanshikaCodes@users.noreply.github.com>
2024-06-22 08:11:03 +0000

    for of loop

---
+++ 05_loops/02_for_of_loop.js
@@ -0,0 +1,37 @@
+// for of
+
+// const arr = [1, 2, 3, 4, 5, 10]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// let greeting = "Hello World!"
+// for (const greet of greeting) {
+//     console.log(greet)
+// }
+
+// Map: Similar to objects but stores unique values only
+const mp = new Map()
+mp.set('IN', "INDIA")
+mp.set('US', "USA")
+mp.set('FR', "FRANCE")
+// console.log(mp)
+// for (const iterator of mp) {                  // Returns array of key value
+//     console.log(iterator)
+// }
+
+// for (const [key, value] of mp) {
+//     console.log(key, " :- ", value)
+// }
+
+// Objects pe for of iterable nhi hote
+// const newObj = {
+//     name : "Vanshika",
+//     age : 19,
+//     "location" : "Jaipur",
+//     dob: "25-12-2005"
+// }
+
+// for (const iterator of newObj) {
+//     console.log(iterator);
+// }
