04710129a60f09225cb59a2c15a7ffc7e3250334
VanshikaCodes <153213173+VanshikaCodes@users.noreply.github.com>
2024-06-22 06:50:46 +0000

    All loops

---
+++ 05_loops/01_all_loops.js
@@ -0,0 +1,17 @@
+// const array = [2, 12, 19, 33]
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element);
+// }
+
+let i = 0
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+// let i = 0
+// do {
+//     console.log(i);
+//     i++;
+// }while (i < 10) 
--- 05_loops/01_for.js
+++
@@ -1,5 +0,0 @@
-const array = [2, 12, 19, 33]
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    console.log(element);
-}
