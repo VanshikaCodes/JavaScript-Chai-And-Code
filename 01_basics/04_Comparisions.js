dae352594328fcb45174d51179a3fd1123ebac01
VanshikaCodes <153213173+VanshikaCodes@users.noreply.github.com>
2024-06-24 05:24:04 +0000

    Comparisions

--- 01_basics/04_Comparisions.js
+++ 01_basics/04_Comparisions.js
@@ -8,10 +8,11 @@
 // console.log(null < 0);           //0
 // console.log(null == 0);          //0
 
-// console.log(undefined > 0);      //0
-// console.log(undefined >= 0);     //0
-// console.log(undefined < 0);      //1      //0
-// console.log(undefined == 0);     //0
+console.log(undefined > 0);      //0
+console.log(undefined >= 0);     //0
+console.log(undefined < 0);      //1      //0
+console.log(undefined == 0);     //0
+
 
 // **********     "==="        #########
 console.log("2" === 1) 
